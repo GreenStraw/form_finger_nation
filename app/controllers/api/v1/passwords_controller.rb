71b7b636a9e9a70d971fb1c0636487e9e5e3ab94
Edit Your .gitconfig file in your home directory <edityour@gitconfig.com>
2014-06-02 10:28:47 -0500

    Create API endpoint for resetting password

---
+++ app/controllers/api/v1/passwords_controller.rb
@@ -0,0 +1,23 @@
+class PasswordsController < Devise::PasswordsController
+
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    if successfully_sent?(resource)
+      return render json: {}, status: 200
+    else
+      return render json: {}, status: 422
+    end
+  end
+
+  # PUT /resource/password
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    if resource.errors.empty?
+      return render json: {}, status: 200
+    else
+      return render json: {}, status: 422
+    end
+  end
+
+end
