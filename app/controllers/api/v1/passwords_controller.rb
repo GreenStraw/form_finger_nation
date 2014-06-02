2392540b732d8775996ff16088c7ee61655a8f2e
Edit Your .gitconfig file in your home directory <edityour@gitconfig.com>
2014-06-02 12:37:57 -0500

    Remove debug statements from passwords controller

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -4,14 +4,10 @@ module Api
 
       # POST /resource/password
       def create
-        puts params.inspect
         self.resource = resource_class.send_reset_password_instructions(params)
-        puts resource_params.inspect
         if successfully_sent?(resource)
-          puts "sent success"
           return render json: {}, status: 200
         else
-          puts "sent failed"
           return render json: {}, status: 422
         end
       end
