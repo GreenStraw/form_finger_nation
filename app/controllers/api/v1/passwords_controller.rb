dea60dea32ec8775398d926a9fe7fb88190e5c32
Edit Your .gitconfig file in your home directory <edityour@gitconfig.com>
2014-06-02 11:11:24 -0500

    Add endpoint for resetting password by supplying email

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,23 +1,30 @@
-class PasswordsController < Devise::PasswordsController
+module Api
+  module V1
+    class PasswordsController < Devise::PasswordsController
 
-  # POST /resource/password
-  def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    if successfully_sent?(resource)
-      return render json: {}, status: 200
-    else
-      return render json: {}, status: 422
+      # POST /resource/password
+      def create
+        puts params.inspect
+        self.resource = resource_class.send_reset_password_instructions(params)
+        puts resource_params.inspect
+        if successfully_sent?(resource)
+          puts "sent success"
+          return render json: {}, status: 200
+        else
+          puts "sent failed"
+          return render json: {}, status: 422
+        end
+      end
+
+      # PUT /resource/password
+      def update
+        self.resource = resource_class.reset_password_by_token(params)
+        if resource.errors.empty?
+          return render json: {}, status: 200
+        else
+          return render json: {}, status: 422
+        end
+      end
     end
   end
-
-  # PUT /resource/password
-  def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-    if resource.errors.empty?
-      return render json: {}, status: 200
-    else
-      return render json: {}, status: 422
-    end
-  end
-
 end
