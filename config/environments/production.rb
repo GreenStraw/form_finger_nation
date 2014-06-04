9b4045733c2b3e995df050f5ae9c122a6f2dfbcb
Bryan Newton <bryandotnewton@gmail.com>
2014-06-04 08:26:00 -0500

    production mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ App::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
