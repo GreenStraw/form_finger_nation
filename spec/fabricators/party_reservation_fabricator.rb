69e6d98b62da53802feae7b9ff9bf3a16627af81
Bryan Newton <bryandotnewton@gmail.com>
2014-06-25 12:37:57 -0500

    attendee notification of watch party in three days

--- spec/fabricators/party_reservation_fabricator.rb
+++ spec/fabricators/party_reservation_fabricator.rb
@@ -1,4 +1,9 @@
 Fabricator(:party_reservation) do
+  user { Fabricate(:user) }
+  email { sequence(:email) { |i| "foo#{i}@example.com"} }
+end
+
+Fabricator(:full_party_reservation, from: :party_reservation) do
   party { Fabricate(:party) }
   user { Fabricate(:user) }
   email { sequence(:email) { |i| "foo#{i}@example.com"} }
