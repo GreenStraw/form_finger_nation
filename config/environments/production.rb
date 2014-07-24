3c3036ae352667d8b195e684a8cd1bf53dbca070
Bryan Newton <bryandotnewton@gmail.com>
2014-07-24 11:21:44 -0500

    heroku emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,17 @@ Baseapp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.mandrillapp.com",
+    :port           => 587,
+    :authentication => "plain",
+    :user_name      => ENV["MANDRILL_USERNAME"],
+    :password       => ENV["MANDRILL_PASSWORD"],
+    :domain         => "foam-finger-nation.herokuapp.com",
+    :enable_starttls_auto => true
+  }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,15 +1,5 @@
 ActionMailer::Base.default_url_options = {
-  # :host => ENV['SMTP_DEFAULT_URL']
   :host => ENV['SMTP_DEFAULT_URL']
 }
 ActionMailer::Base.default from: ENV['EMAIL_FROM']
 ActionMailer::Base.raise_delivery_errors = ENV['SMTP_RAISE_DELIVERY_ERRORS']
-ActionMailer::Base.smtp_settings = {
-  :address              => ENV['SMTP_ADDRESS'],
-  :port                 => ENV['SMTP_PORT'],
-  :domain               => ENV['SMTP_DOMAIN'],
-  :user_name            => ENV['SMTP_USER_NAME'],
-  :password             => ENV['SMTP_PASSWORD'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
