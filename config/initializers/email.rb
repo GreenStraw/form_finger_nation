129c2dd9f778bc8869552bacb32892f227ff251a
Russell Sherman <russell.t.sherman@gmail.com>
2014-08-09 14:31:43 -0500

    add required env vars to travis yml

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.default_url_options = {
   :host => ENV['SMTP_DEFAULT_URL']
 }
-ActionMailer::Base.default from: ENV['EMAIL_FROM']
+ActionMailer::Base.default from: ENV['MAILER_SEND_AS']
 ActionMailer::Base.raise_delivery_errors = ENV['SMTP_RAISE_DELIVERY_ERRORS']
