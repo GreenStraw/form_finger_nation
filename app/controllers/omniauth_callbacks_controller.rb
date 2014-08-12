5ac98a8ce0f871027a3d06533050c72659703385
numsweb <numsweb@users.noreply.github.com>
2014-08-12 15:35:21 -0500

    remove unnecessary identity model and migration, clean up the commented code in controller and model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,6 +14,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	end
 
 	  def failure
+      flash[:error] = "We were unable to log you into Facebook with those credentials."
       #handle you logic here..
       #and delegate to super.
       super
@@ -26,40 +27,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	alias_method :github, :all
 	alias_method :passthru, :all
 	alias_method :google_oauth2, :all
-  
-=begin
 
-  def self.provides_callback_for(provider)
-      
-      class_eval %Q{
-        def #{provider}
- 
-          @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
-
-          if @user.persisted?
-            sign_in_and_redirect @user, event: :authentication
-            set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-          else
-            session["devise.#{provider}_data"] = env["omniauth.auth"]
-            redirect_to new_user_registration_url
-          end
-        end
-      }
-    end
-
-
-  #[:twitter, :facebook, :linked_in].each do |provider|
-  #[:facebook].each do |provider| #
-    provides_callback_for :facebook
-    #end
-=end
-=begin
-  def after_sign_in_path_for(resource)
-    if resource.email_verified?
-      super resource
-    else
-      finish_signup_path(resource)
-    end
-  end
-=end
 end
