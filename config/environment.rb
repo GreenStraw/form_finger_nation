04bcca1ebdb7f5be8b939ff2a2e59a695da67bf0
numsweb <numsweb@users.noreply.github.com>
2014-08-12 17:26:14 -0500

    turn on dorenv

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# require 'dotenv' ; Dotenv.load ".env.#{Rails.env}"
+require 'dotenv' ; Dotenv.load ".env.#{Rails.env}"
 
 # Initialize the Rails application.
 Baseapp::Application.initialize!
