861e9faa6793e09338822b13a2eff3167d94f8b0
Russell Sherman <russell.t.sherman@gmail.com>
2014-08-13 21:17:06 -0500

    app helper spec coverage

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,19 +9,20 @@ describe ApplicationHelper do
     }
   end
 
-  describe 'when environment is development and test then site url' do
-    it { helper.site_url.should eql('http://localhost:3000')  }
-  end
-
-  describe 'when environment is production then site url' do
-    it { 
-      Rails.stub(env: ActiveSupport::StringInquirer.new('production'))
-      if Rails.env.test?
-        helper.site_url.should eql('http://localhost:3000')
-      else
-        helper.site_url.should eql('http://www.BaseApp.com/')  
-      end
-    }
+  describe 'site_url' do
+    describe 'when environment is development and test then site url' do
+      it { helper.site_url.should eql('http://localhost:3000')  }
+    end
+    describe 'when environment is production then site url' do
+      it { 
+        Rails.stub(env: ActiveSupport::StringInquirer.new('production'))
+        if Rails.env.test?
+          helper.site_url.should eql('http://localhost:3000')
+        else
+          helper.site_url.should eql('http://www.BaseApp.com/')  
+        end
+      }
+    end
   end
 
   describe 'meta author' do
@@ -35,52 +36,68 @@ describe ApplicationHelper do
   describe 'meta keywords' do
     it { helper.meta_keywords.should eql('Add some keywords here')  }
   end
-
-  describe 'when page title present in full title ' do
-    it { helper.full_title('welcome').should eql('welcome | BaseApp')  }
+  
+  describe 'full_title' do
+    describe 'when page title present in full title ' do
+      it { helper.full_title('welcome').should eql('welcome | BaseApp')  }
+    end
+    describe 'when page title not present in full title' do
+      it { helper.full_title('').should eql('BaseApp')  }
+    end
+  end
+  
+  describe 'flash_type_to_alert' do
+    describe 'when flash type is notice' do
+      it{
+        name = 'notice'
+        flash[:notice]= 'flash #{name.to_s} message'
+        helper.flash_type_to_alert(:notice).should eql(:success)
+      }
+    end
+    describe 'when flash type is info' do
+      it{
+        name = 'info'
+        flash[:info]= 'flash #{name.to_s} message'
+        helper.flash_type_to_alert(:info).should eql(:info)
+      }
+    end
+    describe 'when flash type is alert' do
+      it{
+        name = 'alert'
+        flash[:alert]= 'flash #{name.to_s} message'
+        helper.flash_type_to_alert(:alert).should eql(:warning)
+      }
+    end
+    describe 'when flash type is error' do
+      it{
+        name = 'error'
+        flash[:error]= 'flash #{name.to_s} message'
+        helper.flash_type_to_alert(:error).should eql(:danger)
+      }
+    end
+    describe 'when flash type is not vallid' do
+      it{
+        helper.flash_type_to_alert(:success).should eql(:success)
+      }
+    end
   end
 
-  describe 'when page title not present in full title' do
-    it { helper.full_title('').should eql('BaseApp')  }
-  end
-
-  describe 'when flash type is notice' do
+  describe 'devise_mapping' do
     it{
-      name = 'notice'
-      flash[:notice]= 'flash #{name.to_s} message'
-      helper.flash_type_to_alert(:notice).should eql(:success)
+      helper.devise_mapping.class.should eql(Devise::Mapping)
     }
   end
 
-  describe 'when flash type is info' do
+  describe 'resource_name' do
     it{
-      name = 'info'
-      flash[:info]= 'flash #{name.to_s} message'
-      helper.flash_type_to_alert(:info).should eql(:info)
+      helper.resource_name.should eql(:user)
     }
   end
 
-  describe 'when flash type is alert' do
+  describe 'resource_class' do
     it{
-      name = 'alert'
-      flash[:alert]= 'flash #{name.to_s} message'
-      helper.flash_type_to_alert(:alert).should eql(:warning)
+      helper.resource_class.should eql(User)
     }
   end
-
-  describe 'when flash type is error' do
-    it{
-      name = 'error'
-      flash[:error]= 'flash #{name.to_s} message'
-      helper.flash_type_to_alert(:error).should eql(:danger)
-    }
-  end
-
-  describe 'when flash type is not vallid' do
-    it{
-      helper.flash_type_to_alert(:success).should eql(:success)
-    }
-  end
-
-
+  
 end
