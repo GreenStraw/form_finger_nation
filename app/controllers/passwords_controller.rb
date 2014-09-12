77f83e56ec29513bc1a8d30e0f5218c06a75a481
Bryan Newton <bryandotnewton@gmail.com>
2014-09-12 07:49:48 -0500

    password reset
    redirect to a non-bad place after sending reset email
    
    link in email goes to the right place

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,8 @@
+class PasswordsController < Milia::PasswordsController
+  protected
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_path if is_navigational_format?
+  end
+
+end  # class
