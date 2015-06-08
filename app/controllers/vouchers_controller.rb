449b4aa104c693d5876f80789fe3c433f2d73515
Im Genius <zaeem.asif@gmail.com>
2015-06-08 21:23:25 +0500

    vouchers

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,6 +1,7 @@
 class VouchersController < ApplicationController
   before_action :set_voucher, only: [:show, :edit, :update, :destroy, :redeem_voucher]
-
+  before_action :authenticate_user!
+  
   # GET /vouchers
   def index
     @redeemable_vouchers = current_user.vouchers.redeemable
