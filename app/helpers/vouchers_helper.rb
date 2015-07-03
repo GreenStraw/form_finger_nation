03133fe9480cc67ace355477f89a1e07830aa648
Im Genius <zaeem.asif@gmail.com>
2015-07-03 17:42:06 +0500

    design fix

--- app/helpers/vouchers_helper.rb
+++ app/helpers/vouchers_helper.rb
@@ -2,4 +2,8 @@ module VouchersHelper
   def package_selects
     Package.order(:name).map {|package| [package.name, package.id]}
   end
+
+  def party_selects
+    Party.order(:name).map {|package| [package.name, package.id]}
+  end
 end
