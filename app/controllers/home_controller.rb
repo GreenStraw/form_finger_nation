a2225001925053aeffcbceb5579b3774b15f63cd
Zaeem <zaeem.asif@gmail.com>
2015-07-10 16:35:30 +0500

    redirections bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,9 +36,9 @@ class HomeController < ApplicationController
     if current_user
       @user = current_user
       if current_user.sign_in_count == 1
-        redirect_to root_path
-      else
         redirect_to user_root_path
+      else
+        redirect_to root_path
       end
     else
       @user = User.new
