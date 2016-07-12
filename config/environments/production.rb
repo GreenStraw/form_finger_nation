931539db0aa7f8fb90985be9909bb4d8cf5e81f5
Calvin <calvincarter_1@yahoo.com>
2016-07-11 23:09:19 -0500

    Added sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,11 @@ Baseapp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    :address        => "smtp.mandrillapp.com",
+    :address        => ENV["SMTP_ADDRESS_SENDGRID"],
     :port           => 587,
     :authentication => "plain",
-    :user_name      => ENV["MANDRILL_USERNAME"],
-    :password       => ENV["MANDRILL_PASSWORD"],
+    :user_name      => ENV["SENDGRID_USERNAME"],
+    :password       => ENV["SENDGRID_PASSWORD"],
     :domain         => "foam-finger-nation.herokuapp.com",
     :enable_starttls_auto => true
   }
