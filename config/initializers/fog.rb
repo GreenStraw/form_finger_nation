b3acc079fb761a12eebccdfed7b5420d711e345b
foamfinger1 <rhettlg@yahoo.com>
2017-04-02 23:16:01 -0500

    changed fog for cloud service to new name

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV["AWS_SECRET_KEY_ID"],
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
   }
-  config.fog_directory  = 'foam-finger-nation'                    # required
+  config.fog_directory  = 'foam-finger'                    # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
