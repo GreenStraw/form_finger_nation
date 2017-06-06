41b5e113f50f9e7d7ea1c1d899ecbfddfc3d0199
Calvin <calvincarter_1@yahoo.com>
2017-06-06 17:55:15 -0500

    venue adding user to choose as manager

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -93,7 +93,7 @@ class ApplicationController < ActionController::Base
   # def does_user_have_access_vendor_view
   #    unless current_user.admin || current_user.has_role?(:venue_manager)
   #    redirect_to root_url
-	# end
-  end
+  # end
+  #end
 
 end
