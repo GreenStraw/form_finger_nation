515bbb0e599a5472e350d7635b903ca2371476a3
Calvin <calvincarter_1@yahoo.com>
2017-06-06 17:30:29 -0500

    venue adding user to choose as manager

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -42,7 +42,7 @@ class VenuesController < ApplicationController
       # @user = User.find(current_user.id)
       if !current_user.has_role?(:venue_manager, @venue)
         role = Role.create(name: 'manager', resource_id: @venue.id, resource_type: "Venue")
-        UsersRole.create(user_id: @venue.user_id, role_id: role.id)
+        UsersRole.create(user_id: params[:user_id], role_id: role.id)
         # current_user.roles << role
         # current_user.add_role(:venue_manager, @venue)
       end
