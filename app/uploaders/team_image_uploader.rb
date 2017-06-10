c56ee074a730d3924773302753bd0e434b78bff8
rhett <rhettlg@yahoo.com>
2017-06-10 09:07:54 -0500

    sports icons

--- app/uploaders/team_image_uploader.rb
+++ app/uploaders/team_image_uploader.rb
@@ -7,7 +7,7 @@ class TeamImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "images/sport/#{model.id}"
+    "images/sports/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
