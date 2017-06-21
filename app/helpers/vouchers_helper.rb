d4a47e21c0f95e0e22e2a269ee23bc0f74b7829b
Calvin <calvincarter_1@yahoo.com>
2017-06-21 04:31:27 -0500

    adding general package coupon

--- app/helpers/vouchers_helper.rb
+++ app/helpers/vouchers_helper.rb
@@ -1,7 +1,7 @@
 module VouchersHelper
-	
+
   def package_selects
-    Package.order(:name).where(for_everyone: false).map {|package| [package.name, package.id]}
+    Package.order(:name).where(for_everyone: false, venue_id: Venue.id).map {|package| [package.name, package.id]}
   end
 
   def party_selects
