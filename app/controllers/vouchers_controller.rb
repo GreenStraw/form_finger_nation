351672a0adbb25a4c5f6af2171a128fafa6d4cae
Calvin <calvincarter_1@yahoo.com>
2017-07-05 11:28:23 -0500

    new voucher route

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,6 +1,6 @@
 class VouchersController < ApplicationController
   before_action :set_voucher, only: [:show, :edit, :update, :destroy, :redeem_voucher]
-  before_action :set_package, only: [:new]
+  #before_action :set_package, only: [:new]
   before_action :authenticate_user!
 
   load_and_authorize_resource :venue, only: [:new]
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Baseapp::Application.routes.draw do
   patch 'user/:id' => 'account#update_profile_picture', :as => 'update_profile_picture'
   put 'user/user_loc' => 'account#user_loc'
 
-  resources :vouchers
+  get 'venues/:id/new/:id', :to => "venues#new", :as => "video_image"
+
+  #resources :vouchers
   resources :packages, except: [:new, :create] do
     member do
       put 'assign'
