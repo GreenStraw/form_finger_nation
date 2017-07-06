73d044ca8e698a04efb69b998475e9f577ea4e5f
Calvin <calvincarter_1@yahoo.com>
2017-07-05 21:54:38 -0500

    new voucher route

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,10 +1,8 @@
 class VouchersController < ApplicationController
   before_action :set_voucher, only: [:show, :edit, :update, :destroy, :redeem_voucher]
-  before_action :set_package, only: [:new]
   before_action :authenticate_user!
 
   load_and_authorize_resource :venue, only: [:new]
-  load_and_authorize_resource :package
   
   # GET /vouchers
   def index
@@ -61,10 +59,6 @@ class VouchersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_voucher
       @voucher = Voucher.find(params[:id])
-    end
-
-    def set_package
-      @package = Package.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
