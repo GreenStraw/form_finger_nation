96589f231c1e4e38e1dd34ae6bc98574a5f7c75a
Calvin <calvincarter_1@yahoo.com>
2017-07-05 21:00:11 -0500

    new voucher route

--- config/routes.rb
+++ config/routes.rb
@@ -73,7 +73,7 @@ Baseapp::Application.routes.draw do
     resource :vouchers, only: [:create]
 
     resources :vouchers, only: [:new] do
-        get 'vouchers/new/:id' => 'vouchers#set_package', :as => 'vouchers'
+        get 'new/:id' => 'vouchers#set_package', :as => 'vouchers'
     end
   end
   resource :account, :controller => :account
