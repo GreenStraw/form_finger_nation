bec7b2065564d387f19282691c87fbbb49ea9055
Calvin <calvincarter_1@yahoo.com>
2017-07-06 19:58:52 -0500

    new voucher route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Baseapp::Application.routes.draw do
   patch 'user/:id' => 'account#update_profile_picture', :as => 'update_profile_picture'
   put 'user/user_loc' => 'account#user_loc'
 
-  get 'venues/:id/packages/:pk_id/vouchers/new' => 'vouchers#new', :as => 'new_vouchers'
+  get 'venues/:id/packages/:package_id/vouchers/new' => 'vouchers#new', :as => 'new_vouchers'
 
   resources :vouchers
   resources :packages, except: [:new, :create] do
