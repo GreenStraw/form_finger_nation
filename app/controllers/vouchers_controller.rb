15bb75aaacff7b7ba18e19bece39aeea02fb004e
Calvin <calvincarter_1@yahoo.com>
2017-07-07 03:56:05 -0500

    new voucher route

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,7 +1,7 @@
 class VouchersController < ApplicationController
   before_action :set_voucher, only: [:show, :edit, :update, :destroy, :redeem_voucher]
   before_action :set_package, only: [:new, :create]
-  before_action :set_venue, only: [:new, :create]
+  #before_action :set_venue, only: [:new, :create]
   before_action :authenticate_user!
   load_and_authorize_resource :user
 
@@ -65,13 +65,13 @@ class VouchersController < ApplicationController
       @voucher = Voucher.find(params[:id])
     end
 
-    def set_package
-      @package = Package.find('71')
-    end
+    #def set_package
+    #  @package = Package.find('71')
+    #end
 
-    def set_venue
-      @venue = Package.find('141')
-    end
+    #def set_venue
+    #  @venue = Package.find('141')
+    #end
 
     # Only allow a trusted parameter "white list" through.
     def voucher_params
