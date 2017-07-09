02a8138775443d7e7bcbb73a9ecccc7209797588
Calvin <calvincarter_1@yahoo.com>
2017-07-09 13:51:19 -0500

    adding ffn name

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -15,10 +15,10 @@ class VenuesController < ApplicationController
     # Amount in cents
     #@amount = 500
 
-    #customer = Stripe::Customer.create(
-    #  :email => params[:stripeEmail],
-    #  :source  => params[:stripeToken]
-    #)
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
 
     charge = Stripe::Charge.create(
       customer: '111',
