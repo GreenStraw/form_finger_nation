6685dac023be14cae967ca8eefc3afe684f3c0c0
Calvin <calvincarter_1@yahoo.com>
2017-07-23 22:53:13 -0500

    pending party filter test

--- app/models/party.rb
+++ app/models/party.rb
@@ -71,13 +71,13 @@ class Party < ActiveRecord::Base
     end
   end
 
-  def get_pending_parties
+  def self.get_pending_parties
     venues = Venue.where(id: current_user.roles.where("name = 'venue_manager' OR  name = 'manager'").map(&:resource_id))
     
     venues.try(:each) do |venue|
       @pending_parties.concat(venue.parties.where('parties.organizer_id != ? ', current_user.id) )
     end
-    
+
   end
 
   def unregistered_attendees
