6cd32aad117b470ab00d16f389819945ad107109
rhett <rhettlg@yahoo.com>
2017-07-26 00:47:25 -0500

    new logic to render vendors and not parties

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -28,7 +28,7 @@ class VenuesController < ApplicationController
     #)
 
       @test = Venue.where(id: current_user.roles.where("name = 'venue_manager' OR  name = 'manager'").map(&:resource_id))
-      #@test.joins(:party).where('parties.venue_id is null')
+      @test.joins(:party).where('parties.venue_id is null')
 
     respond_with @venues
   end
