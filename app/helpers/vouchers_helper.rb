fe7ffc6dc278322b5f29e9513273e2e4e4dbcbf8
Calvin <calvincarter_1@yahoo.com>
2017-07-29 13:47:14 -0500

    filter party creating voucher

--- app/helpers/vouchers_helper.rb
+++ app/helpers/vouchers_helper.rb
@@ -4,7 +4,22 @@ module VouchersHelper
     Package.order(:name).where(for_everyone: false, venue_id: venue, id: package).map {|package| [package.name, package.id]}
   end
 
-  def party_selects(venue)
-    Party.order(:name).where(venue_id: venue).map {|package| [package.name, package.id]}
+  def party_selects(current_user)
+
+  	venues =  Venue.where(id: current_user.roles.where("name = 'venue_manager' OR  name = 'manager'").map(&:resource_id))
+
+    pending_parties  = []
+
+    venues.try(:each) do |venue|
+
+      if current_user.admin?
+        pending_parties.concat(venue.parties)
+      else
+        pending_parties.concat(venue.parties.where('parties.organizer_id != ? ', current_user.id).map {|package| [package.name, package.id]} )
+      end
+    end
+
+    return pending_parties
+
   end
 end
