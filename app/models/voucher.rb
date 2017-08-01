91413ceae54bfa3dfe95b27cd364a17862ff9d1d
Calvin <calvincarter_1@yahoo.com>
2017-08-01 00:54:34 -0500

    new filter for redeemed vouchers

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -60,8 +60,10 @@ class Voucher < ActiveRecord::Base
 
   end
 
-  def self.redeemed(current_user)
-    where("redeemed_at IS NOT ? AND user_id = ?", nil, current_user.id)
+  def self.redeemed
+    redeem = where("redeemed_at IS NOT NULL")
+    return redeem
+    #where("redeemed_at IS NOT ? AND user_id = ?", nil, current_user.id)
   end
 
 
