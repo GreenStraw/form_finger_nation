097e900efe96373cee4fbc9f963557cdc1b3633b
Calvin <calvincarter_1@yahoo.com>
2017-08-03 04:23:42 -0500

    new venue logic for dropdown

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,6 +1,6 @@
 
 
-module VouchersHelper
+module VenuesHelper
   def venue_selects 
     Venue.order(:name).map {|venue| [venue.name, "#{venue.name} (#{venue.address.street1} #{venue.address.city}, #{venue.address.state})"]}
   end
