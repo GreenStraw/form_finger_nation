dbb5b4c252423241c5f3f02c8c28acceb1caa0ac
Calvin <calvincarter_1@yahoo.com>
2017-08-03 04:17:19 -0500

    new venue logic for dropdown

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -3,7 +3,7 @@ module PartiesHelper
     Team.order(:name).map {|team| [team.name, team.id]}
   end
   def venue_selects 
-    Venue.order(:name).map {|venue| [venue.name, venue.id]}
+    Venue.order(:name).map {|venue| [venue.name, venue.address]}
   end
   def reservations_include?(party, user)
     user.party_reservations.where(user_id: user.id, party_id: party.id).first.blank? ? false : true
---
+++ app/helpers/venues_helper.rb
@@ -0,0 +1,7 @@
+
+
+module VouchersHelper
+  def venue_selects 
+    Venue.order(:name).map {|venue| [venue.name, "#{venue.name} (#{venue.address.street1} #{venue.address.city}, #{venue.address.state})"]}
+  end
+end
