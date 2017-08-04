c78cc3c51e133cff9c71ed88a9dcefe72b435134
Calvin <calvincarter_1@yahoo.com>
2017-08-04 08:12:00 -0500

    create watch party redesign

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -26,11 +26,12 @@ class Venue < ActiveRecord::Base
   end
 
   def upcoming_parties
-    self.parties.where('scheduled_for >= ? && verified = ?', Time.now, 1).order(:scheduled_for)
+    self.parties.where('scheduled_for >= ?', Time.now).order(:scheduled_for)
+    #self.parties.where('scheduled_for >= ? && verified = ?', Time.now, 1).order(:scheduled_for)
   end
 
   def past_parties
-    self.parties.where('scheduled_for < ? && verified = ?', Time.now, 1).order(:scheduled_for)
+    self.parties.where('scheduled_for < ?', Time.now).order(:scheduled_for)
   end
 
   def name_and_address
