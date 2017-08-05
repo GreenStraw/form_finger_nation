16a7fc959ae865cb415d5df99994511ffca209b6
Calvin <calvincarter_1@yahoo.com>
2017-08-05 16:11:46 -0500

    create watch party redesign

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -207,7 +207,7 @@ class PartiesController < ApplicationController
     
     if @party = Party.create(party_params)
 
-      @party.scheduled_for("2017-09-08 13:30")
+      @party.scheduled_for="2017-09-08 13:30"
 
       flash[:notice] = 'Party was successfully created.'
       @party.save(:validate => false)
