67181b885294c9f20d5a623e36168e519c27b8b5
Calvin <calvincarter_1@yahoo.com>
2017-08-05 17:28:47 -0500

    create watch party redesign

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -8,4 +8,10 @@ module PartiesHelper
   def reservations_include?(party, user)
     user.party_reservations.where(user_id: user.id, party_id: party.id).first.blank? ? false : true
   end
+  def timeOfParty
+	 [{:value => 5, :text => "05" }, {:value => 6, :text => "06" }, {:value => 7, :text => "07" }, 
+	  {:value => 8, :text => "08" }, {:value => 9, :text => "09" }, {:value => 10, :text => "10" },
+	  {:value => 11, :text => "11" },{:value => 12, :text => "12" },{:value => 1, :text => "01" },
+	  {:value => 2, :text => "02" }, {:value => 3, :text => "03" }, {:value => 4, :text => "04" } ].map {|d| [d.value, d.text]}
+  end
 end
