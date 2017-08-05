850d23678cc3733b639eacacac56f5212e30084e
Calvin <calvincarter_1@yahoo.com>
2017-08-05 11:18:24 -0500

    create watch party redesign

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -193,8 +193,10 @@ class PartiesController < ApplicationController
     end
 
     to_date = party_params[:scheduled_for]
-    date_s = to_date.to_s << ' ' << '13:00' << ':00'
-    party_params[:scheduled_for] = Date.strftime(date_s, "%Y-%m-%d %H:%M:%S %Z") # DateTime.parse(date_s) # DateTime.parse(date_s)
+    date_s = to_date.to_s << 'T' << '04:05:06' << '+07:00'
+    party_params[:scheduled_for] = DateTime.strptime(date_s, '%Y-%m-%dT%H:%M:%S%z') # DateTime.parse(date_s) # DateTime.parse(date_s)
+
+    
 
     #Date.strftime(date_s, "%Y-%m-%d %H:%M:%S %Z")
     #Date.strptime(updated,"%a, %d %m %Y %H:%M:%S %Z")
