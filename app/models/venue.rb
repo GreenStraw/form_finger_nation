ff8710cfeed061299f098c75ff6ee96b2bb3c13b
Calvin <calvincarter_1@yahoo.com>
2017-08-06 07:28:29 -0500

    query for upcoming parties

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -26,8 +26,8 @@ class Venue < ActiveRecord::Base
   end
 
   def upcoming_parties
-    #self.parties.where('scheduled_for >= ?', Time.now).order(:scheduled_for)
-    self.parties.where('scheduled_for >= ? && verified = ?', Time.now, true)
+    self.parties.where('scheduled_for >= ?', Time.now).order(:scheduled_for)
+    #self.parties.where('scheduled_for >= ? && verified = ?', Time.now, true).order(:scheduled_for)
   end
 
   def past_parties
