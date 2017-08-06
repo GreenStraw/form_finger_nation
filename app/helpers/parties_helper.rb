491a0d8a56e8c643d913606be4b46aa0a8a31b52
Calvin <calvincarter_1@yahoo.com>
2017-08-05 19:04:38 -0500

    create watch party redesign

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -9,20 +9,9 @@ module PartiesHelper
     user.party_reservations.where(user_id: user.id, party_id: party.id).first.blank? ? false : true
   end
   def timeOfParty
-	 timeList = [{"value" => 5,  "text" => "05" }, {"value" => 6, "text" => "06" }, {"value" => 7, "text" => "07" }, 
+	 [{"value" => 5,  "text" => "05" }, {"value" => 6, "text" => "06" }, {"value" => 7, "text" => "07" }, 
 	  {"value" => 8,  "text" => "08" }, {"value" => 9, "text" => "09" }, {"value" => 10, "text" => "10" },
 	  {"value" => 11, "text" => "11" }, {"value" => 12,"text" => "12" }, {"value" => 1, "text" => "01" },
-	  {"value" => 2,  "text" => "02" }, {"value" => 3, "text" => "03" }, {"value" => 4,"text" => "04" } ]
-
-	times  = []
-
-    timeList.try(:each) do |t|
-
-    	times.concat([t.value, t.text])
-
-    end
-
-    return times
-
+	  {"value" => 2,  "text" => "02" }, {"value" => 3, "text" => "03" }, {"value" => 4,"text" => "04" } ].map {|d| [d[0].value	, d[0].text]}
   end
 end
