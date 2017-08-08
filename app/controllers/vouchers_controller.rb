8f0a9501e80ca89d8ce406988788a799791a2da3
Calvin <calvincarter_1@yahoo.com>
2017-08-08 08:08:59 -0500

    commneted out unused code on party show page

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -31,8 +31,8 @@ class VouchersController < ApplicationController
 
     @voucher = Voucher.new(voucher_params)
 
-    #party_organizer_id = Party.where('parties.id = ? ', @voucher[:party_id]).map(&:organizer_id).first
-    #@voucher.update_attribute(:user_id, party_organizer_id)
+    party_organizer_id = Party.where('parties.id = ? ', @voucher[:party_id]).map(&:organizer_id).first
+    @voucher.update_attribute(:user_id, party_organizer_id)
 
     if @voucher.save
       redirect_to @voucher, notice: 'Voucher was successfully created.'
