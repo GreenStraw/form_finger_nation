f9158b9be0370bbecea3b0d0e294437e359901b5
Calvin <calvincarter_1@yahoo.com>
2017-08-10 09:00:42 -0500

    venue social icons 1.fb 2. twitter

--- app/models/team.rb
+++ app/models/team.rb
@@ -49,6 +49,12 @@ class Team < ActiveRecord::Base
     self.parties.where('scheduled_for > ?', Time.now).order(:scheduled_for)
   end
 
+  def team_upcoming_parties(ip_zipcode, database_zipcode)
+
+    self.parties.where('scheduled_for > ?', Time.now).order(:scheduled_for)
+    
+  end
+
   private
 
   def ensure_address
