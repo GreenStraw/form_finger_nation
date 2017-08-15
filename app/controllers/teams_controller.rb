11a8c8b836f05d11d91eea57416304b088947b7f
Calvin <calvincarter_1@yahoo.com>
2017-08-15 06:30:11 -0500

    parties in my area test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -52,7 +52,7 @@ class TeamsController < ApplicationController
   # GET /teams/1
   def show
 
-    @teams_within_area = @team.geo_search(0,0,30,@team.id)
+    @teams_within_area = Team.geo_search(0,0,30,@team.id)
 
 
     #@my_local_team_parties = Team.team_upcoming_parties(request.location.zip_code ,current_user.address.zip)
