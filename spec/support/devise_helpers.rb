5279e1f3121809595f5350757455733e77fa4058
Calvin <calvincarter_1@yahoo.com>
2017-08-18 06:50:32 -0500

    adding longitude vars to current_user object

--- spec/support/devise_helpers.rb
+++ spec/support/devise_helpers.rb
@@ -1,20 +1,4 @@
 module DeviseHelpers
 
 
-
-  def logout
-    sign_out current_user if current_user
-    @current_user = nil
-    request.env['warden'].stub(:authenticate!).and_throw(:warden, {:scope => :user})
-    controller.stub :current_user => nil
-  end
-
-  def current_user
-    @current_user
-  end
-
-  def should_deny_access
-    response.should redirect_to(root_url)
-  end
-
 end
