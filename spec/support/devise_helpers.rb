413a247c1345a603427b9e456898a4acc590f024
Calvin <calvincarter_1@yahoo.com>
2017-08-18 05:26:38 -0500

    adding longitude vars to current_user object

--- spec/support/devise_helpers.rb
+++ spec/support/devise_helpers.rb
@@ -4,12 +4,6 @@ module DeviseHelpers
     if role.nil?
       request.env['warden'].stub(:authenticate!).and_throw(:warden, {:scope => :user})
       controller.stub :current_user => nil
-    else
-      @current_user = user = Fabricate(:user)
-      @current_user.add_role role
-      sign_in @current_user
-      request.env['warden'].stub :authenticate! => @current_user
-      controller.stub :current_user => @current_user
     end
   end
 
@@ -21,7 +15,7 @@ module DeviseHelpers
   end
 
   def current_user
-
+    @current_user
   end
 
   def should_deny_access
