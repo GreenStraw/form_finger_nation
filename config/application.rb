a1353d79f74af343fd5807317cc1bc060793dbc8
Calvin L Carter <SZ85Y2@onstar.ad.gm.com>
2017-08-30 12:49:56 -0500

    time +5

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Baseapp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    #config.time_zone = 'Central Time (US & Canada)'
-    config.active_record.default_timezone = :utc #:local Or :utc
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local #:local Or :utc
 
     config.action_controller.include_all_helpers = true
 
