96f5b575a9eac748c6b97accb409cddcc85e5a5e
Calvin <calvincarter_1@yahoo.com>
2017-09-02 13:02:23 -0500

    edit venue - update manager

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -93,7 +93,7 @@ class VenuesController < ApplicationController
 
          if role.any?
             user_role = UsersRole.where("role_id =?", role.first)
-            user_role.update_attribute(:user_id, params[:user_id])
+            user_role.update_all(:user_id => params[:user_id])
          end
 
       end
