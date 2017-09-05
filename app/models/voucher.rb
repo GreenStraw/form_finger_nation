f4f1363a8295d09d7d61ebfa286ebd603a4d3409
Calvin <calvincarter_1@yahoo.com>
2017-09-05 07:12:39 -0500

    voucher page new query to render redeemed and unredeemed vouchers

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -49,7 +49,8 @@ class Voucher < ActiveRecord::Base
               voucher_copy = where("party_id = ? AND package_id = ?", partyid, pkgid).first
               voucher_copy.assign_attributes(:user_id  => current_user.id, :redeemed_at => nil)
               voucher << voucher_copy
-          
+          else
+              voucher << user_redeemed_voucher
           end
 
       #end
