3eec36fe7299b58a9705a720773e6344b227eab0
Calvin <calvincarter_1@yahoo.com>
2017-09-24 21:42:21 -0500

    rename databses file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+puts "Calvin CArter"
+
 set :application, 'baseapp'
 set :user, 'baseapp'
 
@@ -26,7 +28,7 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{.env.production config/databases.yml}
+set :linked_files, %w{.env.production config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
