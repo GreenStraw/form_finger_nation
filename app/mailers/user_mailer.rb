be3e94698e9a1b8ba0afd2a766effcdeac5f0960
Calvin <calvincarter_1@yahoo.com>
2017-11-05 14:39:07 -0600

    added default url for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,24 +3,30 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @to = user.email
     @user = user
-    @url = "https://www.foamfingernation.com/login"
-    @bcc = ["calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    @url = "http://174.138.110.237/login"
+    #@url = "https://www.foamfingernation.com/login"
+    #@bcc = ["calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    @bcc = ["calvincarter.160@gmail.com"]
     mail(to: @to, bcc: @bcc, from: '"Foam Finger Nation" <Info@foamfingernation.com>', subject: 'Welcome to Foam Finger Nation!')
   end
 
   def alumni_group_email(user)
     @to = user.email
-    @bcc = ["alumnigroups@foamfingernation.com","calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    #@bcc = ["alumnigroups@foamfingernation.com","calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    @bcc = ["calvincarter.160@gmail.com"]
     @user = user
-    @url = "https://www.foamfingernation.com/login"
+    @url = "http://174.138.110.237/login"
+    #@url = "https://www.foamfingernation.com/login"
     mail(to: @to, bcc: @bcc, from: '"Foam Finger Nation" <Alumnigroups@foamfingernation.com>', subject: 'Welcome to Foam Finger Nation!')
   end
 
   def venue_email(user)
     @to = user.email
-    @bcc = ["Sportsbars@foamfingernation.com", "calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    #@bcc = ["Sportsbars@foamfingernation.com", "calvincarter.160@gmail.com", "sayrooj@gmail.com", "rhettlg@yahoo.com"]
+    @bcc = ["calvincarter.160@gmail.com"]
     @user = user
-    @url = "https://www.foamfingernation.com/login"
+    @url = "http://174.138.110.237/login"
+    #@url = "https://www.foamfingernation.com/login"
     mail(to: @to, bcc: @bcc, from: '"Foam Finger Nation" <Sportsbars@foamfingernation.com>', subject: 'Welcome to Foam Finger Nation!')
   end
 end
