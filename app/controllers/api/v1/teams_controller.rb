3a4efe9fc64df3f8fe344daa83afe6b1377148bf
Calvin <calvincarter_1@yahoo.com>
2017-11-18 15:15:24 -0600

    deploying apis

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,5 +1,14 @@
 module Api
   module V1 
+    class TeamsController < ApplicationController
+      load_and_authorize_resource :user
+      load_and_authorize_resource :team
 
+      def index
+        team = Team.all
+        render json: {status: 'SUCCESS', message:'Loaded articles', data:team}, status:ok
+      end
+
+    end
   end
 end
