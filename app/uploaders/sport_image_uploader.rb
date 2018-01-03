16d885b5cd12194650dd260fc2dcefcf8ebcf58a
Calvin <calvincarter_1@yahoo.com>
2018-01-02 18:37:51 -0600

    sport images directory adjustment

--- app/uploaders/sport_image_uploader.rb
+++ app/uploaders/sport_image_uploader.rb
@@ -7,7 +7,7 @@ class SportImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "images"
+    "images/#{model.class.to_s.underscore}".strip
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
